b6b68d486622f47d9c7875e2dd490e92c3c785ce
Klapeyron <tomasz.bartos@protonmail.com>
2018-05-05 18:12:22 +0200

    Unified include dir paths

--- modules/Findrclcpp.cmake
+++ modules/Findrclcpp.cmake
@@ -1,12 +1,6 @@
-find_path(RCLCPP_INCLUDE_DIR NAMES rclcpp)
+find_path(RCLCPP_INCLUDE_DIRS NAMES rclcpp)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(rclcpp DEFAULT_MSG RCLCPP_INCLUDE_DIR)
-
-if(RCLCPP_FOUND)
-  set(RCLCPP_INCLUDE_DIRS ${RCLCPP_INCLUDE_DIR})
-else()
-  message(FATAL_ERROR "rclcpp package not found")
-endif(RCLCPP_FOUND)
+find_package_handle_standard_args(rclcpp DEFAULT_MSG RCLCPP_INCLUDE_DIRS)
 
 mark_as_advanced(RCLCPP_INCLUDE_DIRS)
--- modules/Findrxcpp.cmake
+++ modules/Findrxcpp.cmake
@@ -1,12 +1,6 @@
-find_path(RXCPP_INCLUDE_DIR NAMES rxcpp)
+find_path(RXCPP_INCLUDE_DIRS NAMES rxcpp)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(rxcpp DEFAULT_MSG RXCPP_INCLUDE_DIR)
-
-if(RXCPP_FOUND)
-  set(RXCPP_INCLUDE_DIRS ${RXCPP_INCLUDE_DIR})
-else()
-  message(FATAL_ERROR "rxcpp package not found")
-endif(RXCPP_FOUND)
+find_package_handle_standard_args(rxcpp DEFAULT_MSG RXCPP_INCLUDE_DIRS)
 
 mark_as_advanced(RXCPP_INCLUDE_DIRS)
