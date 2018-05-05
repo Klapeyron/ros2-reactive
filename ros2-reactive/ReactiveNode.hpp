b3e2e20c4f2547a8bbb31c87ece48d0634eb25c3
Klapeyron <tomasz.bartos@protonmail.com>
2018-05-05 16:07:03 +0200

    Temporary documentation

--- ros2-reactive/ReactiveNode.hpp
+++ ros2-reactive/ReactiveNode.hpp
@@ -12,11 +12,37 @@
 
 namespace ros2_reactive
 {
+
+/**
+ * Wrapper for ROS2 node, which allows for creating reactive observables.
+ * More information about reactive interface can be found in {@link ReactiveX.RxCpp}.
+ * @see [ReactiveX.RxCpp](https://github.com/ReactiveX/RxCpp)
+ */
 class ReactiveNode : public rclcpp::Node
 {
  public:
+  /**
+   * Using-declaration moving rclcpp::Node constructors into this scope.
+   * Class can be constructed directly from any rclcpp::Node constructor.
+   */
   using rclcpp::Node::Node;
 
+  /**
+   * Creates reactive observable from given topic.
+   *
+   * @tparam MessageType Type of ROS2 message in given topic.
+   * @tparam TopicType Representation of ROS2 topic
+   *     (same as in rclcpp::Node::create_subscription).
+   * @tparam Args All additional parameters which will be forwarded to
+   *     rclcpp::Node::create_subscription function.
+   *
+   * @param topic ROS2 topic (same as in rclcpp::Node::create_subscription).
+   * @param args All additional parameters of rclcpp::Node::create_subscription
+   *     function, which will be forwarded (apart from topic and callback) to
+   *     create_subscription function.
+   *
+   * @return Reactive observable of given MessageType and topic.
+   */
   template <typename MessageType, typename TopicType, typename... Args>
   rxcpp::observable<MessageType> create_observable(TopicType&& topic,
                                                    Args&&... args)
